7f88a7768bf8d69cd3fc52b4adb36ded26660b0c
SandraKoch <sandraannakoch@gmail.com>
2023-07-14 22:17:59 +0200

    basic styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,19 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101',
+        color: 'white',
+        backgroundColor: 'black',
+        backgroundSize: 'cover',
+        listStyle: 'none',
+        backgroundImage: `linear-gradient(
+          to bottom,
+          rgba(47, 48, 58, 0.4) 0%,
+          rgba(47, 48, 58, 0.4) 50%
+        ), url("https://img.freepik.com/free-photo/coffee-beans-with-props-making-coffee_1220-4536.jpg?w=1060&t=st=1689365325~exp=1689365925~hmac=abb2e77ae526b6358a1a95533c279c4e8616d91b88a26b18c71b121817db94f2")`,
       }}
     >
       <FeedbackComponent />
